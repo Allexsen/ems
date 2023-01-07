44d370a306a6b13802ee2f0a5f8aa19abe56db42
Alex <asvanidze12@gmail.com>
2023-01-07 14:51:59 +0400

    DB initialization

--- database/database.go
+++ database/database.go
@@ -1,3 +1,10 @@
-package database
+package database // configure database connection to the system
 
-// configure database connection to the system
+import (
+	connection "github.com/Allexsen/ems/database/connect"
+)
+
+func init() {
+	connection.ConnectDB(500, 100, 100, 2)
+	// db := connection.GetDB()
+}
