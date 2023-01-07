03593b141d3f513eebeedff324dfe4db0635d549
Alex <asvanidze12@gmail.com>
2023-01-07 19:08:22 +0400

    Route updates

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -16,18 +16,34 @@ func Initialize(router *gin.Engine) {
 		c.Redirect(301, "/auth")
 	})
 
-	auth := r.Group("/auth")
+	ref := r.Group("/referral")
 	{
-		auth.GET("", func(c *gin.Context) {
-			c.String(200, "Login")
+		ref.GET("/validate", func(c *gin.Context) {
+			c.String(200, "Input Your Referral Code")
 		})
 
-		auth.GET("/validate", func(c *gin.Context) {
-			c.String(200, "Validate referral")
+		ref.POST("/validate/:referral", func(c *gin.Context) {
+			c.String(200, "Referral Validation")
 		})
 
-		auth.GET("/register", func(c *gin.Context) {
-			c.String(200, "Register")
+		ref.GET("/invalid", func(c *gin.Context) {
+			c.String(200, "Invalid Referral Code")
+		})
+
+		ref.GET("/new", func(c *gin.Context) {
+			c.String(200, "Generate Referral Code")
+		})
+
+		ref.GET("/terminate", func(c *gin.Context) {
+			c.String(200, "Active Referrals List")
+		})
+
+		ref.DELETE("/terminate/:referral", func(c *gin.Context) {
+			c.String(200, "Referral Terminated")
+		})
+
+		ref.GET("/register", func(c *gin.Context) {
+			c.String(200, "Registration")
 		})
 	}
 
