95c50b9c76f5c34ef17d608c71e6809ff3aad8a7
Alex <asvanidze12@gmail.com>
2023-01-07 18:40:05 +0400

    Routing with placeholders

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -10,15 +10,47 @@ var (
 
 func Initialize(router *gin.Engine) {
 	r = router
+	gin.ForceConsoleColor()
+
 	r.GET("/", func(c *gin.Context) {
 		c.Redirect(301, "/auth")
 	})
 
 	auth := r.Group("/auth")
-	auth.Use(gin.Logger())
 	{
-		auth.GET("/", func(c *gin.Context) {
-			c.String(200, "Successfully rerouted")
+		auth.GET("", func(c *gin.Context) {
+			c.String(200, "Login")
+		})
+
+		auth.GET("/validate", func(c *gin.Context) {
+			c.String(200, "Validate referral")
+		})
+
+		auth.GET("/register", func(c *gin.Context) {
+			c.String(200, "Register")
+		})
+	}
+
+	profile := r.Group("/profile")
+	{
+		profile.GET("", func(c *gin.Context) {
+			c.String(200, "Self Profile")
+		})
+
+		profile.GET("/:pid", func(c *gin.Context) {
+			c.String(200, "Employee Profile")
+		})
+
+		profile.GET("/:pid/history", func(c *gin.Context) {
+			c.String(200, "Employee History")
+		})
+
+		profile.GET("/:pid/reviews", func(c *gin.Context) {
+			c.String(200, "Employee Reviews")
+		})
+
+		profile.POST("/:pid/reviews/add", func(c *gin.Context) {
+			c.String(200, "Add Review")
 		})
 	}
 
