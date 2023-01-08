11fce294c6392277555014b61c7522e6bd0d4e4b
Alex <asvanidze12@gmail.com>
2023-01-08 12:05:57 +0400

    Referral validation

---
+++ api/middlewares/referral/ref-validate.go
@@ -0,0 +1,24 @@
+package referral
+
+import (
+	"net/http"
+
+	"github.com/Allexsen/ems/database"
+	"github.com/gin-gonic/gin"
+)
+
+func ValidateReferral(referral string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := database.GetDB()
+		q := "SELECT used FROM referrals WHERE code=" + referral
+		row := db.QueryRow(q)
+
+		var isUsed bool
+		err := row.Scan(&isUsed)
+		if err != nil || isUsed {
+			c.String(http.StatusUnauthorized, "Invalid referral")
+		}
+
+		c.Next()
+	}
+}
